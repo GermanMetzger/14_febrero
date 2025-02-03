a646d867d6fbfa54fd5efb0653aa2683dd3cb15c
German <=metzgergerman@gmail.com>
2025-02-03 02:46:47 -0300

    terminado

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
-{\rtf1}
+
+
+const sonido = document.getElementById('miSonido');
+
+let teclasPresionadas = {};
+
+// Detectar las teclas que se presionan
+document.addEventListener('keydown', function(event) {
+    // Verificar si la tecla presionada es un número (0-9)
+    if (event.key >= '0' && event.key <= '9') {
+        const sonidoCopia = sonido.cloneNode();
+        sonidoCopia.play();
+    }
+});
+
